2cda964f26f2fc85cddc46e58a5891737e53ddb2
Lucky <963190000@qq.com>
2019-01-02 13:22:00 +0800

    上传启动服务器功能

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -10,3 +10,25 @@ var server = require('gulp-webserver'); //起服务
 var devUglify = require('gulp-uglify'); //压缩js
 var cleanCss = require('gulp-clean-css'); //压缩css
 
+// 起服务
+gulp.task('server', () => {
+    return gulp.src('./src')
+        .pipe(server({
+            port: 3333,
+            open: true,
+            livereload: true,
+        }))
+})
+
+// 编译sass 压缩css
+// gulp
+
+// 合并js
+
+// 压缩js
+
+// 5 watch 监听js压缩 css编译
+
+// 默认执行任务
+
+// build 任务 js css 生成到dist文件夹
